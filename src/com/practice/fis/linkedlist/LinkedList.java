c5446894450e0bb4e16dc9cfa6558dc4be2e8dd0
Shridhar M Patil <mpshridhar2016@gmail.com>
2020-11-15 00:14:10 -0500

    Update LinkedList.java

--- src/com/practice/fis/linkedlist/LinkedList.java
+++ src/com/practice/fis/linkedlist/LinkedList.java
@@ -23,9 +23,9 @@ public class LinkedList{
 			head = node;
 		} else {
 			Node temp = head;
-			while (temp.next != null)
+			while (temp.next != null){
 				temp = temp.next;
- 
+                             }
 			temp.next = node;
 		}
 	}
